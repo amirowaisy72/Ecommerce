43485bbd775ad7180065b623fdeb8a543a91c68d
amirowaisy72 <amirowaisy72@gmail.com>
2023-12-04 16:45:53 +0500

    keyword add button

--- Admin/frontend/src/Components/Pages/Products/AddProduct/Keywords/Index.js
+++ Admin/frontend/src/Components/Pages/Products/AddProduct/Keywords/Index.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Index = () => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [keywords, setKeywords] = useState([]);
 
   const handleInputChange = (e) => {
@@ -9,26 +9,26 @@ const Index = () => {
   };
 
   const handleKeyPress = (e) => {
-    if (e.key === ' ' && inputValue.trim() !== '') {
+    if (e.key === " " && inputValue.trim() !== "") {
       // Add the keyword when space is pressed
       setKeywords((prevKeywords) => [...prevKeywords, inputValue.trim()]);
-      setInputValue(''); // Clear the input field
+      setInputValue(""); // Clear the input field
     }
   };
 
   return (
     <div>
-      <div style={{ marginBottom: '10px' }}>
+      <div style={{ marginBottom: "10px" }}>
         <strong>Keywords:</strong>
         {keywords.map((keyword, index) => (
           <span
             key={index}
             style={{
-              display: 'inline-block',
-              backgroundColor: '#e0e0e0',
-              borderRadius: '5px',
-              padding: '5px',
-              margin: '0 5px',
+              display: "inline-block",
+              backgroundColor: "#e0e0e0",
+              borderRadius: "5px",
+              padding: "5px",
+              margin: "0 5px",
             }}
           >
             {keyword}
@@ -42,8 +42,11 @@ const Index = () => {
         onChange={handleInputChange}
         onKeyUp={handleKeyPress}
         placeholder="Type a keyword"
-        style={{ marginBottom: '10px' }}
+        style={{ marginBottom: "10px" }}
       />
+      <button onClick={handleKeyPress} className="btn btn-success">
+        Add Keyword
+      </button>
     </div>
   );
 };
